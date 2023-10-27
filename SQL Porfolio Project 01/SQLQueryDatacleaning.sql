b6df56e3e0d94f713dfb050acb8ee1889fc083dc
Mrhau95 <91181712+Mrhau95@users.noreply.github.com>
2023-10-27 14:21:03 +0700

    Add files via upload

---
+++ SQL Porfolio Project 01/SQLQueryDatacleaning.sql
@@ -0,0 +1,169 @@
+﻿/* cleaning data in SQL queries
+*/
+
+
+
+-------------------------------------------------------------------
+--1/Format date
+
+select SaleDate, convert(date, SaleDate) -- convert sang kiểu date
+ from PorfolioProject..NashvilleHousing
+ 
+--thêm colum 
+alter table NashvilleHousing
+add SaleDateconvert Date;
+--cập nhật colum vào bảng
+ update NashvilleHousing
+ set SaleDateconvert = convert(date, SaleDate)
+ --xuất ra colum đã convert
+ select SaleDateconvert  
+ from
+ NashvilleHousing
+
+ -- xóa colum date Saledate cũ
+ alter table NashvilleHousing
+ drop column SaleDate;
+
+ select * from
+ NashvilleHousing
+ 
+
+ -----------------------------------------------------------
+ --Populate Property Addres data
+  select * from
+ NashvilleHousing
+ where PropertyAddress is null
+
+ --Check với mối ParcellID sẽ tương ứng đúng với Propertyaddress hay k? và nếu các propertyaddress is null thì tìm cách điền nó vào đúng với ParcellID
+
+
+ --query này sẽ cho ta tháy được rõ ràng các giá trị null sẽ trùng với ParcellID và có địa chỉ
+  select a.ParcelID, a.PropertyAddress, b.ParcelID, b.PropertyAddress , isnull(a.PropertyAddress,b.PropertyAddress)
+  from
+      NashvilleHousing as a
+ join NashvilleHousing as b
+	on a.ParcelID = b.ParcelID
+	and a.[UniqueID ]<>b.[UniqueID ]
+ where a.PropertyAddress is null
+
+ --update value Propertyaddress từ b vào a để k còn value null
+ update a
+ set PropertyAddress = isnull(a.PropertyAddress,b.PropertyAddress)
+ from
+      NashvilleHousing as a
+ join NashvilleHousing as b
+	on a.ParcelID = b.ParcelID
+	and a.[UniqueID ]<>b.[UniqueID ]
+ where a.PropertyAddress is null
+
+
+
+ --------------------------------------
+ --Breaking out Address into individual Columns (Address, City, State)
+ --Tách các địa chỉ và thành phố 
+  select 
+  SUBSTRING (PropertyAddress, 1, CHARINDEX(',',PropertyAddress)-1) as Address, -- k láy ra dấu ,
+  SUBSTRING (PropertyAddress,CHARINDEX(',',PropertyAddress)+ 1, len(PropertyAddress)) --láy từ dấu , trở đi 
+
+  from NashvilleHousing
+ 
+--cập nhật thêm colum addres mới
+alter table NashvilleHousing
+add PropertySplitAddres nvarchar(255);
+--cập nhật colum vào bảng
+ update NashvilleHousing
+ set PropertySplitAddres = SUBSTRING (PropertyAddress, 1, CHARINDEX(',',PropertyAddress)-1)
+
+alter table NashvilleHousing
+add PropertySplitCity nvarchar(255);
+--cập nhật colum vào bảng
+ update NashvilleHousing
+ set PropertySplitCity = SUBSTRING (PropertyAddress,CHARINDEX(',',PropertyAddress)+ 1, len(PropertyAddress))
+
+ ---xuất ra sẽ ra 3 colum và PropertyAddress đã được tách ra
+ select PropertyAddress, PropertySplitAddres, PropertySplitCity
+ from
+ PorfolioProject..NashvilleHousing
+
+
+
+
+ -------------xử lý colum owerAddress-----------------------
+ 
+select OwnerAddress
+from
+PorfolioProject..NashvilleHousing
+
+--Tách thành 3 colum khác nhau
+select PARSENAME(REPLACE(OwnerAddress,',','.'),3),
+PARSENAME(REPLACE(OwnerAddress,',','.'),2),
+PARSENAME(REPLACE(OwnerAddress,',','.'),1)
+
+from PorfolioProject..NashvilleHousing
+
+ 
+ --cập nhật thêm colum addres mới
+alter table NashvilleHousing
+add OwnerSplitAddres nvarchar(255);
+--cập nhật colum vào bảng
+ update NashvilleHousing
+ set OwnerSplitAddres = PARSENAME(REPLACE(OwnerAddress,',','.'),3)
+
+ alter table NashvilleHousing
+add OwnerSplitCity nvarchar(255);
+--cập nhật colum vào bảng
+ update NashvilleHousing
+ set OwnerSplitCity = PARSENAME(REPLACE(OwnerAddress,',','.'),2)
+
+ alter table NashvilleHousing
+add OwnerSplitStates nvarchar(255);
+--cập nhật colum vào bảng
+ update NashvilleHousing
+ set OwnerSplitStates = PARSENAME(REPLACE(OwnerAddress,',','.'),1)
+
+ --xuất ra các colum vừa tạo mới
+ select OwnerAddress,OwnerSplitAddres, OwnerSplitCity, OwnerSplitStates
+ from
+ PorfolioProject..NashvilleHousing
+
+
+
+ -----------------------
+ --chane Y and N to yes and No in " sold as vacant"
+ select distinct(SoldAsVacant)
+ from
+  PorfolioProject..NashvilleHousing
+
+  --check xem số lượng theo từng value
+ select distinct(SoldAsVacant), count(SoldAsVacant)
+ from
+ PorfolioProject..NashvilleHousing
+ group by SoldAsVacant
+ order by 2 asc
+
+ --viết câu truy vấn để thay đổi nếu value là Y thành yes và N thành No
+ select SoldAsVacant,
+	 case when SoldAsVacant ='Y' then 'Yes'
+	      when SoldAsVacant ='N' then 'No'
+	      else SoldAsVacant
+	      end
+ from
+ PorfolioProject..NashvilleHousing
+
+ --phải update vào thì nó mới thành công
+ update NashvilleHousing
+ set SoldAsVacant = case when SoldAsVacant ='Y' then 'Yes'
+	      when SoldAsVacant ='N' then 'No'
+	      else SoldAsVacant
+	      end
+
+
+
+-----------------------------
+--Remote Duplicates
+
+--------------
+--delete unused Columns
+
+alter table PorfolioProject..NashvilleHousing
+drop column 
---
+++ SQL Porfolio Project 01/SQLQueryDataexploration.sql
@@ -0,0 +1,171 @@
+﻿select *
+from PorfolioProject..CovidDeaths
+order by 3,4
+
+
+ 
+ --select *
+--from PorfolioProject..CovidVaccinations
+--order by 3,4
+
+
+--select data that we are going to be using
+select location, date, total_cases, new_cases, total_deaths, population
+from PorfolioProject..CovidDeaths
+order by 1,2 -- sẽ tháy được số người chết tăng theo thời gian từ tháng 2 trở đi
+
+
+--Xem tỉ lệ phần trăm giữa total_case và total_deaths
+select location, date, total_cases, total_deaths, (total_deaths/total_cases)*100 as totaldethpercentace
+from PorfolioProject..CovidDeaths
+where location like '%ietnam%'and continent is not null
+order by 1,2 
+
+
+--xem phần trăm tổng số ca mắc so với dân số
+select location, date, total_cases, population, (total_deaths/population)*100 as totalpercentage
+from PorfolioProject..CovidDeaths
+where location like '%ietnam%' and continent is not null
+order by 1,2 
+
+
+select location, date, total_cases, population, (total_cases/population)*100 as totalpercentage
+from PorfolioProject..CovidDeaths
+--where continent is not null
+order by 1,2 
+
+
+--nhìn vào các quốc gia với số ca nhiễm cao so với population
+
+select location,population ,max(total_cases) as highestìnection,  max((total_cases/population))*100 as totalpercentage -- láy ra totalcase lớn nhất và tỉ lệ phần trăm cao nhất giữa dân số và total case
+from PorfolioProject..CovidDeaths
+where continent is not null
+group by location, population
+order by totalpercentage  desc
+
+
+--let's break things down by continent
+select continent ,max (cast (total_deaths as int)) as totaldeathcount
+from PorfolioProject..CovidDeaths
+where continent is not null
+group by continent
+order by totaldeathcount   desc
+
+
+
+--thể hiện các quốc gia có số lượng người chết cao nhất
+select location ,max (cast (total_deaths as int)) as totaldeathcount
+from PorfolioProject..CovidDeaths
+where continent is null
+group by location
+order by totaldeathcount   desc
+
+
+
+--showing continents with the highest death count per population
+
+select continent ,max (cast (total_deaths as int)) as totaldeathcount
+from PorfolioProject..CovidDeaths
+where continent is not null
+group by continent
+order by totaldeathcount   desc
+
+
+--globals numbers
+select  date, sum(new_cases) as totalnewcase --tổng số case mới theo thời gian
+from PorfolioProject..CovidDeaths
+where continent is not null
+group by date
+order by 1,2 
+
+select  date, sum(new_cases)as totalnewcase, sum(cast(new_deaths as int)) as totalnewdeath, sum(cast(new_deaths as int))/sum(new_cases)*100 as deathspercentage
+from PorfolioProject..CovidDeaths
+where continent is not null
+group by date
+order by 1,2 
+
+
+select   sum(new_cases)as totalnewcase, sum(cast(new_deaths as int)) as totalnewdeath, sum(cast(new_deaths as int))/sum(new_cases)*100 as deathspercentage
+from PorfolioProject..CovidDeaths
+where continent is not null
+order by 1,2 
+
+
+--JOIN 2 BẢNG
+--LOOKING AT TOTAL POPULATION VS VACCINATIONS
+select dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations
+from PorfolioProject..CovidDeaths as dea
+join PorfolioProject..CovidVaccinations as vac
+	on dea.location = vac.location
+	and dea.date = vac.date
+where dea.continent is not null
+order by 1,2,3
+
+--Rollingpeoplevaccinated: partition by sum sẽ cộng dồn
+select dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations, 
+sum(convert(int,vac.new_vaccinations)) over (partition by dea.location order by dea.location, dea.date) as Rollingpeoplevaccinated
+from PorfolioProject..CovidDeaths as dea
+join PorfolioProject..CovidVaccinations as vac
+	on dea.location = vac.location
+	and dea.date = vac.date
+where dea.continent is not null
+--order by 2,3
+
+
+--use CTE : số cột trong bảng cte phải trùng với số cột trong query đưa vào)
+with PopvsVAC (continent, location, date, population, new_vaccinations, Rollingpeoplevaccinated) 
+
+as (
+
+select dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations, 
+sum(convert(int,vac.new_vaccinations)) over (partition by dea.location order by dea.location, dea.date) as Rollingpeoplevaccinated
+from PorfolioProject..CovidDeaths as dea
+join PorfolioProject..CovidVaccinations as vac
+	on dea.location = vac.location
+	and dea.date = vac.date
+where dea.continent is not null
+--order by 2,3
+)
+select * , (Rollingpeoplevaccinated/population)*100 
+from PopvsVAC
+
+
+--temp table
+drop table if exists #Percentapopulationvaccintaged
+create table #Percentapopulationvaccintaged
+(ontinent nvarchar(255), 
+location nvarchar(255), 
+date datetime, 
+population numeric, 
+new_vaccinations numeric, 
+Rollingpeoplevaccinated numeric
+)
+
+insert into #Percentapopulationvaccintaged
+select dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations, 
+sum(convert(int,vac.new_vaccinations)) over (partition by dea.location order by dea.location, dea.date) as Rollingpeoplevaccinated
+from PorfolioProject..CovidDeaths as dea
+join PorfolioProject..CovidVaccinations as vac
+	on dea.location = vac.location
+	and dea.date = vac.date
+where dea.continent is not null
+--order by 2,3
+
+
+select *, (Rollingpeoplevaccinated/population)*100 as percentapopulationvaccintaged
+from #Percentapopulationvaccintaged
+
+
+
+--create view to store data for later visualizations
+create view Percentapopulationvaccintaged as
+select dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations, 
+sum(convert(int,vac.new_vaccinations)) over (partition by dea.location order by dea.location, dea.date) as Rollingpeoplevaccinated
+from PorfolioProject..CovidDeaths as dea
+join PorfolioProject..CovidVaccinations as vac
+	on dea.location = vac.location
+	and dea.date = vac.date
+where dea.continent is not null
+--order by 2,3
+
+select * from Percentapopulationvaccintaged
